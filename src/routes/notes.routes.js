731c709355ee76efef305c15066f29800aa88867
roman-laptop <roman.calderin.25@gmail.com>
2022-10-18 21:23:22 -0500

    :sparkles: :hammer:

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -1,3 +1,11 @@
 const express = require("express");
 const router = express.Router();
+router.get("/notes/new-notes", (req, res) => {
+    res.render("notes/form-notes");
+});
+
+router.post("/save-notes", (req, res) => {
+    const { title, description } = req.body;
+    res.send("OK");
+});
 module.exports = router;
